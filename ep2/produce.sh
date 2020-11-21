08bf2a0aff0c123c648702b3344662ac405ba62f
Svilen Ivanov <svilen.ivanov@gmail.com>
2020-11-22 00:22:02 +0200

    Finalize the produce script

--- ep2/produce.sh
+++ ep2/produce.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-#VIDEO_OUTPUT=media/videos/scene/480p15
-VIDEO_OUTPUT=media/videos/scene/2160p60
+VIDEO_OUTPUT=./
 rm -f output.mkv
 ffmpeg \
-  -i $VIDEO_OUTPUT/TitleScreen.mp4 \
-  -i $VIDEO_OUTPUT/SortingScene.mp4 \
-  -i $VIDEO_OUTPUT/StudentsSortingScene.mp4 \
-  -i $VIDEO_OUTPUT/DivideAndConquer.mp4 \
-  -i $VIDEO_OUTPUT/Complexity.mp4 \
-  -i $VIDEO_OUTPUT/MainAlgo.mp4 \
-  -i $VIDEO_OUTPUT/MergeSort.mp4 \
-  -i $VIDEO_OUTPUT/Final.mp4 \
+  -i $VIDEO_OUTPUT/TitleScreen.mkv \
+  -i $VIDEO_OUTPUT/Description.mkv   \
+  -i $VIDEO_OUTPUT/BruteForce.mkv \
+  -i $VIDEO_OUTPUT/Greedy.mkv \
+  -i $VIDEO_OUTPUT/OptSubstr.mkv \
+  -i $VIDEO_OUTPUT/Algo.mkv \
+  -i $VIDEO_OUTPUT/DynamicProgramming.mkv \
+  -i $VIDEO_OUTPUT/MinChangeStepByStep.mkv \
   -filter_complex \
   '[0:0] [0:1] [1:0] [1:1] [2:0] [2:1] [3:0] [3:1] [4:0] [4:1] [5:0] [5:1] [6:0] [6:1] [7:0] [7:1]
    concat=n=8:v=1:a=1 [v] [am] ;
-   amovie=bg_7min.flac,volume=0.1 [bg_track] ;
+   amovie=bg_long.flac,volume=0.1 [bg_track] ;
    [am] amerge=inputs=1 [a_stereo] ;
    [a_stereo]loudnorm[a_stereo_norm] ;
    [a_stereo_norm]volume=2[aamp] ;
@@ -24,9 +23,3 @@ ffmpeg \
    -threads 4 \
    -vcodec libx264 -acodec libopus -shortest \
   output.mkv
-# -filter_complex "amovie=bg.flac:loop=0,volume=0.03[audio];[0:a]volume=1.2[sa];[sa][audio]amix[fa]" \
-
-# -map 0:v -map [fa] \
-# out.mp4
-
-
