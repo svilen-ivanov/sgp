a0dc1eeb10de09710a602d6b833b1a4a0ec53040
Svilen Ivanov <svilen.ivanov@gmail.com>
2020-11-22 14:13:16 +0200

    Push some more files that were not included

---
+++ ep2/export_4k.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+export PYTHONPATH=../
+
+#QUALITY=-ql
+QUALITY=-qk
+rm -rf media
+manim scenes/title_scene.py TitleScreen $QUALITY
+manim scenes/description.py Description $QUALITY
+manim scenes/brute_force.py BruteForce $QUALITY
+manim scenes/greedy.py Greedy $QUALITY
+manim scenes/dynamic_programming.py DynamicProgramming $QUALITY
+manim scenes/opt_substr.py OptSubstr $QUALITY
+manim scenes/algo.py Algo $QUALITY
+manim scene.py MinChangeStepByStep $QUALITY
